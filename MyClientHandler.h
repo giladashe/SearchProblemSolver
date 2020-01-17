38194198f43c4ba32ff3ec0a8da9442f15025b11
shaiyis <shaiyis@gmail.com>
2020-01-17 12:25:50 +0200

    server recieves matrix solution

--- MyClientHandler.h
+++ MyClientHandler.h
@@ -26,43 +26,47 @@ public:
     }
 
     virtual void handleClient(int clientSocket) {
-        // initialize buffer to 0
-        char buffer[10000] = {0};
+        bool stop = false;
         string between_lines;
         string allMatrix; // with start & end points
 
-        // read
-        int readData = read(clientSocket, buffer, 10000);
-        int i = 0;
-        while (i < readData) {
-            if (between_lines == "end"){
-                if (this->_cm->hasSolution(allMatrix)) {
-                    int is_sent = send(clientSocket, this->_cm->getSolution(allMatrix).c_str(),
-                                       this->_cm->getSolution(allMatrix).length(), 0);
-                    if (is_sent == -1) {
-                        cout << "Error sending message" << endl;
+        while (!stop){
+            // initialize buffer to 0
+            char buffer[1024] = {0};
+            // read
+            int readData = read(clientSocket, buffer, 1024);
+            int i = 0;
+            while (i < readData) {
+                if (between_lines == "end"){
+                    stop = true;
+                    if (this->_cm->hasSolution(allMatrix)) {
+                        int is_sent = send(clientSocket, this->_cm->getSolution(allMatrix).c_str(),
+                                           this->_cm->getSolution(allMatrix).length(), 0);
+                        if (is_sent == -1) {
+                            cout << "Error sending message" << endl;
+                        }
                     }
-                }
-                else{
-                    Solution solution = this->_solver->solve(allMatrix);
-                    this->_cm->saveSolution(allMatrix, solution);
-                    int is_sent = send(clientSocket, this->_cm->getSolution(allMatrix).c_str(),
-                                       this->_cm->getSolution(allMatrix).length(), 0);
-                    if (is_sent == -1) {
-                        cout << "Error sending message" << endl;
+                    else{
+                        Solution solution = this->_solver->solve(allMatrix);
+                        this->_cm->saveSolution(allMatrix, solution);
+                        int is_sent = send(clientSocket, this->_cm->getSolution(allMatrix).c_str(),
+                                           this->_cm->getSolution(allMatrix).length(), 0);
+                        if (is_sent == -1) {
+                            cout << "Error sending message" << endl;
+                        }
                     }
+                    close(clientSocket);
                 }
-                close(clientSocket);
-            }
-            if (buffer[i] == '\n'){
-                allMatrix.append(between_lines);
-                allMatrix.append("\n");
-                between_lines = "";
+                if (buffer[i] == '\n'){
+                    allMatrix.append(between_lines);
+                    allMatrix.append("\n");
+                    between_lines = "";
+                    i++;
+                    continue;
+                }
+                between_lines += buffer[i];
                 i++;
-                continue;
             }
-            between_lines += buffer[i];
-            i++;
         }
     }
 };
